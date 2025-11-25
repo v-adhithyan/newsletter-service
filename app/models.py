d215d98570d0ded97d9b02c96403e62f593b7935
Adhithyan Vijayakumar <aavispeaks@gmail.com>
2025-11-25 09:48:02 +0530

    optimise content model

--- app/models.py
+++ app/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 # Create your models here.
 class Topic(models.Model):
     name = models.CharField(max_length=200, unique=True)
@@ -7,16 +8,19 @@ class Topic(models.Model):
     def __str__(self):
         return self.name
 
+
 class Content(models.Model):
     topic = models.ForeignKey(Topic, on_delete=models.PROTECT)
     text = models.TextField(blank=False, null=False)
     send_at = models.DateTimeField(auto_now_add=False, blank=False, null=False)
+    is_sent = models.BooleanField(default=False)
+    sent_at = models.DateTimeField(auto_now_add=False, blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
-
     def __str__(self):
         return f'{self.topic} - {self.text}'
+
 
 class User(models.Model):
     email = models.EmailField(unique=True, blank=False, null=False)
