b6fddb5fa52e88967d058bd9d9d0d124cd52f0a6
Adhithyan Vijayakumar <aavispeaks@gmail.com>
2025-11-26 09:40:51 +0530

    add logic to create newsletter content with subscriber ids

--- app/admin.py
+++ app/admin.py
@@ -1,11 +1,53 @@
 from django.contrib import admin
+from django.shortcuts import get_object_or_404
+from django.urls import reverse
+from django.utils.html import format_html
+from django.utils.http import urlencode
 
-from .models import Topic, Subscriber
+from .forms import ContentAdminForm
+from .models import Topic, Subscriber, Content
 
 admin.site.site_header = "Newsletter Service Administration"
 admin.site.site_title = "Newsletter Service Admin"
 admin.site.index_title = "Welcome to Newsletter Service Administration"
 
 # Register your models here.
-admin.site.register(Topic)
 admin.site.register(Subscriber)
+
+
+@admin.register(Topic)
+class TopicAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'send_newsletter_email')
+    search_fields = ('name',)
+
+    def send_newsletter_email(self, topic):
+        app_label = Content._meta.app_label
+        model_name = Content._meta.model_name
+        add_url = reverse('admin:%s_%s_add' % (app_label, model_name))
+        params = {'topic': str(topic.id)}
+        url = f'{add_url}?{urlencode(params)}'
+        return format_html('<a class="button" href="{}">Send email</a>', url)
+
+    send_newsletter_email.short_description = "Send newsletter email"
+    send_newsletter_email.allow_tags = True
+
+
+@admin.register(Content)
+class ContentAdmin(admin.ModelAdmin):
+    form = ContentAdminForm
+    list_display = ('id', 'topic', 'text', 'send_at', 'is_sent')
+    list_filter = ('topic', 'is_sent')
+    search_fields = ('topic', 'text')
+
+    # when send newsletter email action is clicked in topic admin,
+    # we redirect to this with params containing topic id,
+    # use it to display subscriber emails
+    def get_changeform_initial_data(self, request):
+        initial = super().get_changeform_initial_data(request) or {}
+        topic = request.GET.get('topic')
+        if topic:
+            initial['topic'] = topic
+            topic_obj = get_object_or_404(Topic, id=topic)
+            recipient_ids = topic_obj.subscribers.values_list('id', flat=True)
+            initial['recipients'] = recipient_ids
+        return initial
--- app/forms.py
+++ app/forms.py
@@ -1,2 +1,45 @@
+from django.core.exceptions import ValidationError
+from django.forms import ModelForm
+from django.shortcuts import get_object_or_404
+
+from app.models import Content, Subscriber
 
 
+class ContentAdminForm(ModelForm):
+    class Meta:
+        model = Content
+        fields = '__all__'
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        data = kwargs.get('data', None)
+        topic = None
+        if data and 'topic' in data:
+            topic_id = int(data.get('topic', 0))
+            from app.models import Topic
+            topic = get_object_or_404(Topic, id=topic_id)
+        else:
+            if self.instance and self.instance.pk:
+                topic = self.instance.topic
+            else:
+                topic_id = self.initial.get('topic') if hasattr(self, 'initial') else None
+                if topic_id:
+                    from app.models import Topic
+                    topic = get_object_or_404(Topic, id=topic_id)
+
+        if topic:
+            self.fields['recipients'].queryset = Subscriber.objects.filter(topic=topic)
+        else:
+            self.fields['recipients'].queryset = Subscriber.objects.none()
+
+    def clean(self):
+        cleaned = super().clean()
+        topic = cleaned.get('topic')
+        recipients = cleaned.get('recipients')
+        if recipients and topic:
+            invalid = [r.email for r in recipients if r.topic_id != topic.id]
+            if invalid:
+                raise ValidationError(
+                    "Selected recipients do not all belong to the chosen topic: %s" % ", ".join(invalid))
+        return cleaned
